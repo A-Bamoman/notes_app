7bc6992e4cc70d525cdbe10b332aa61ecc30f100
A-Bamoman <aalliiaallii972@gmail.com>
2023-07-23 01:23:34 +0300

    edit note view ui done

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -3,20 +3,23 @@ import 'package:flutter/material.dart';
 import 'custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({Key? key}) : super(key: key);
-
+  const CustomAppBar({Key? key, required this.title, required this.icon}) : super(key: key);
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return  Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
-          'Notes',
-          style: TextStyle(
+          title,
+          style: const TextStyle(
             fontSize: 28,
           ),
         ),
-        CustomIcon(),
+        CustomIcon(
+           icon: icon,
+        ),
       ],
     );
   }
